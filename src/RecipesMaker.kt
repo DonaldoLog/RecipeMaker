1402af86127f514a4c5b6596f91bcb585ef21029
donaldolog <donaldolog@gmail.com>
2019-08-09 18:02:02 -0500

    fun

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,21 +1,15 @@
 import javafx.beans.binding.When
 import java.lang.NumberFormatException
+var receta: MutableList<String> =  mutableListOf<String>()
 
 fun main (args: Array<String>) {
-    val saludo = ":: Bienvenido a Recipe Maker ::"
     var ingredientes: List<String> = listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "HUevos", "Aceite")
-    var receta: MutableList<String> =  mutableListOf<String>()
+    val saludo = ":: Bienvenido a Recipe Maker ::"
     var response: Int? = null
     var ingrediente: Int? = null
-    val opciones: String = """
-        Selecciona la opción deseada
-        1. Hacer una receta
-        2. Ver mi receta
-        3. Salir
-    """.trimIndent()
     println(saludo)
     while (response != 3) {
-        println(opciones)
+        makeRecipe()
         try {
             response = readLine()?.toInt()?:0
             when(response) {
@@ -48,7 +42,7 @@ fun main (args: Array<String>) {
                     }
 
                 }
-                2 ->println(receta)
+                2 -> viewRecipe()
                 3 -> println("Adios:)")
                 else -> {
                     println("Ingrese una opcion valida")
@@ -63,4 +57,18 @@ fun main (args: Array<String>) {
 
 
 
+}
+
+fun makeRecipe () {
+    val opciones: String = """
+        Selecciona la opción deseada
+        1. Hacer una receta
+        2. Ver mi receta
+        3. Salir
+    """.trimIndent()
+    println(opciones)
+}
+
+fun viewRecipe() {
+    println(receta)
 }
